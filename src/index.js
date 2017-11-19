a3f8d5bfa91996be030c6b6b6edd2b938ade552b
ssgxsdmapping <ssgxsdmapping@gmail.com>
2017-11-19 14:11:38 +0100

    initialisation webpack
    initialisation webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const test = require('./test');
+
+/**
+ * Materilize dependences
+ */
+import materializeCss from '../node_modules/materialize-css/dist/css/materialize.css';
+require('../node_modules/materialize-css/dist/js/materialize.js');
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        },{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },]
+    }
+};
+
